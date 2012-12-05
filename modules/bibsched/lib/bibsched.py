bf9c15e0cce012741599753a3bd628c9c5f95a43
Alessio Deiana <alessio.deiana@cern.ch>
2012-12-06 00:18:57 +0100

    BibSched: fixes for sleep_task

--- modules/bibsched/lib/bibsched.py
+++ modules/bibsched/lib/bibsched.py
@@ -1085,7 +1085,7 @@ class BibSched(object):
             for other_task_id, other_proc, other_runtime, other_status, other_priority, other_host, other_sequenceid in self.mono_tasks_all_nodes:
                 if priority < other_priority:
                     # Sleep ourselves
-                    sleep_task(task_id, proc, runtime, status, priority, host, sequenceid)
+                    sleep_task(task_id, proc, priority, status, sequenceid)
                     return True
 
         elif (task_id, proc, runtime, status, priority, host, sequenceid) in self.node_relevant_waiting_tasks:
